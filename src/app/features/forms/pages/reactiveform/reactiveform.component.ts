7e6c78f275ec69bc97359d55e4ab2c4e87e563f2
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-02-09 07:31:29 +0100

    Init form builder

--- src/app/features/forms/pages/reactiveform/reactiveform.component.ts
+++ src/app/features/forms/pages/reactiveform/reactiveform.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-reactiveform',
-  templateUrl: './reactiveform.component.html',
-  styleUrls: ['./reactiveform.component.scss']
+  selector: "app-reactiveform",
+  templateUrl: "./reactiveform.component.html",
+  styleUrls: ["./reactiveform.component.scss"]
 })
 export class ReactiveformComponent implements OnInit {
+  checkoutForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { SharedHeaderComponent } from './components/shared-header/shared-header.component';
 
 @NgModule({
   declarations: [SharedHeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [SharedHeaderComponent]
 })
 export class SharedModule {}
